e8b9fb2042f77afd8bed5e5c7097b7b2021cc3ff
Pedro Aguilar <pedro@heru-app.com>
2020-12-08 00:09:00 -0600

    [TESTS] additional tests

--- tests/test_recommender_models.py
+++ tests/test_recommender_models.py
@@ -1,3 +1,6 @@
+from pandas import DataFrame
+from sqlalchemy import text
+
 from api.schemas.enums import RecommenderType
 from src import RatingBasedRecommender, OpenRateBasedRecommender
 
@@ -19,3 +22,36 @@ def test_rating_recommender_type():
 
     assert recommender.type == RecommenderType.rating
 
+
+def test_open_rate_recommender_type():
+    recommender = OpenRateBasedRecommender(1)
+
+    assert recommender.type == RecommenderType.open_rate
+
+
+def test_open_rate_model_rated_articles_gets_df(populate_db, engine, monkeypatch):
+    def mock_query(*args, **kwargs):
+        _sql = """
+            SELECT * FROM articles_article;
+        """
+        return text(_sql)
+
+    recommender = OpenRateBasedRecommender(1)
+    monkeypatch.setattr(recommender, '_query', mock_query)
+    rated_articles = recommender.rated_articles(engine)
+
+    assert isinstance(rated_articles, DataFrame)
+
+
+def test_rating_model_rated_articles_gets_df(populate_db, engine, monkeypatch):
+    def mock_query(*args, **kwargs):
+        _sql = """
+            SELECT * FROM articles_article;
+        """
+        return text(_sql)
+
+    recommender = RatingBasedRecommender(1)
+    monkeypatch.setattr(recommender, '_query', mock_query)
+    rated_articles = recommender.rated_articles(engine)
+
+    assert isinstance(rated_articles, DataFrame)
